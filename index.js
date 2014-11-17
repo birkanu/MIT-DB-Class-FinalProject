45d746750fdcf62167369666df9205ece90a8f85
Birkan Uzun <birkanuzun27@gmail.com>
2014-11-17 17:50:10 -0500

    Add Socket.io Library

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express')
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
