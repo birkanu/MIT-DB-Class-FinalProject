320fac234e997f6c852ae91d20517eeba871f6bc
Birkan Uzun <birkanuzun27@gmail.com>
2014-11-17 22:13:35 -0500

    Implement Sample Tweet Counter

--- index.js
+++ index.js
@@ -1,20 +1,43 @@
-var express = require('express')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app = require('express')();
+var http = require('http').Server(app); 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+app.set('port', process.env.PORT || 3000);
+
+http.listen(app.get('port'), function() {  
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+var Twit = require('twit');  
+var T = new Twit({  
+  consumer_key: 'HbbEIcATMEM2SMVIWXbMefIyR',
+  consumer_secret: 'cdQ2Wq54XbF08tZ7EUjw1tmQDtFVowRGszr86BD4JnGzBOLFyV',
+  access_token: '1056098544-hQL3guqW6k6dqETspAYVrjDieJewzxTOqq73G5R',
+  access_token_secret: 'YUSIqPcU9dpXuD16QeI4mjflh3xH7QZ4FhW3NbVDn9Plh'
+});
+
+var io = require('socket.io')(http); 
+
+// filter the public stream by english tweets containing `#apple`
+var appleStream = T.stream('statuses/filter', { track: '#apple', language: 'en' });
+// filter the public stream by english tweets containing `#apple`
+var orangeStream = T.stream('statuses/filter', { track: '#orange', language: 'en' });
+
+io.on('connection', function (socket) { 
+  var appleTweetCount = 0;
+  var orangeTweetCount = 0;
+  appleStream.on('tweet', function(tweet) {
+  	appleTweetCount += 1;
+    socket.emit('appleTweet', { count: appleTweetCount});
+  });
+  orangeStream.on('tweet', function(tweet) {
+    orangeTweetCount += 1;
+    socket.emit('orangeTweet', { count: orangeTweetCount});
   });
 });
+
+
 
