19f59cc500aeec4dc6bbc5aff142ebd8c82a9a74
Birkan Uzun <birkanuzun27@gmail.com>
2014-11-18 20:32:33 -0500

    Added D3.js Sample Bar Chart

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app); 
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public'));
 
 http.listen(app.get('port'), function() {  
   console.log('Express server listening on port ' + app.get('port'));
