7cde5ae3754625992e937f89f83521d8fcdf5d70
Birkan Uzun <birkanuzun27@gmail.com>
2014-11-24 01:34:30 -0500

    Add user authentication

--- index.js
+++ index.js
@@ -1,24 +1,168 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app); 
+var express = require('express'),
+    exphbs = require('express-handlebars'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    Twit = require('twit');
 
-app.set('port', process.env.PORT || 3000);
+var config = require('./config.js'), //config file contains all tokens and other private info
+    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
+
+var app = express();
+
+//===============PASSPORT===============
+
+// Passport session setup.
+passport.serializeUser(function(user, done) {
+  console.log("serializing " + user.username);
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  console.log("deserializing " + obj);
+  done(null, obj);
+});
+
+// Use the LocalStrategy within Passport to login/”signin” users.
+passport.use('local-signin', new LocalStrategy(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+  function(req, username, password, done) {
+    funct.localAuth(username, password)
+    .then(function (user) {
+      if (user) {
+        console.log("LOGGED IN AS: " + user.username);
+        req.session.success = 'You are successfully logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT LOG IN");
+        req.session.error = 'Could not log user in. Please try again.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+  }
+));
+
+// Use the LocalStrategy within Passport to register/"signup" users.
+passport.use('local-signup', new LocalStrategy(
+  {passReqToCallback : true}, //allows us to pass back the request to the callback
+  function(req, username, password, done) {
+    funct.localReg(username, password)
+    .then(function (user) {
+      if (user) {
+        console.log("REGISTERED: " + user.username);
+        req.session.success = 'You are successfully registered and logged in ' + user.username + '!';
+        done(null, user);
+      }
+      if (!user) {
+        console.log("COULD NOT REGISTER");
+        req.session.error = 'That username is already in use, please try a different one.'; //inform user could not log them in
+        done(null, user);
+      }
+    })
+    .fail(function (err){
+      console.log(err.body);
+    });
+  }
+));
+
+// Simple route middleware to ensure user is authenticated.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  req.session.error = 'Please sign in!';
+  res.redirect('/signin');
+}
+
+//===============EXPRESS================
+
+// Configure Express
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
 
-http.listen(app.get('port'), function() {  
-  console.log('Express server listening on port ' + app.get('port'));
-});
+// // Session-persisted message middleware
+// app.use(function(req, res, next){
+//   var err = req.session.error,
+//       msg = req.session.notice,
+//       success = req.session.success;
 
+//   delete req.session.error;
+//   delete req.session.success;
+//   delete req.session.notice;
+
+//   if (err) res.locals.error = err;
+//   if (msg) res.locals.notice = msg;
+//   if (success) res.locals.success = success;
+
+//   next();
+// });
+
+// Configure express to use handlebars templates
+var hbs = exphbs.create({
+    defaultLayout: 'main'
+});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//===============ROUTES===============
+
+//displays our homepage
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.render('home', {user: req.user});
 });
 
-var Twit = require('twit');  
+//displays our signup page
+app.get('/signin', function(req, res){
+  res.render('signin');
+});
+
+//sends the request through our local signup strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/local-reg', passport.authenticate('local-signup', {
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//sends the request through our local login/signin strategy, and if successful takes user to homepage, otherwise returns then to signin page
+app.post('/login', passport.authenticate('local-signin', { 
+  successRedirect: '/',
+  failureRedirect: '/signin'
+  })
+);
+
+//logs user out of site, deleting them from the session, and returns to homepage
+app.get('/logout', function(req, res){
+  var name = req.user.username;
+  console.log("LOGGIN OUT " + req.user.username)
+  req.logout();
+  res.redirect('/');
+  req.session.notice = "You have successfully been logged out " + name + "!";
+});
+
+//===============PORT=================
+var port = process.env.PORT || 5000; //select your port or let it pull from your .env file
+var http = require('http').Server(app); 
+http.listen(port);
+console.log("listening on " + port + "!");
+  
 var T = new Twit({  
-  consumer_key: 'fklWWQROAC69xJX337Z8Tpoas',
-  consumer_secret: 'tfC8sodrY2G5ws5CHXscY4wFi8sB8o68E0FFCXeRSfFxN5m0A4',
-  access_token: '1056098544-N42lVlU0OTQWw5S2CtvLZzJoplyrMCDNwl0lPZV',
-  access_token_secret: 'O2f2Zu3xFuMK3Axk7GudKgqq2PFV8GCwDPLBYos3eht7O'
+  consumer_key: config.twitter.consumerKey,
+  consumer_secret: config.twitter.consumerSecret,
+  access_token: config.twitter.accessToken,
+  access_token_secret: config.twitter.accessTokenSecret
 });
 
 var io = require('socket.io')(http); 
